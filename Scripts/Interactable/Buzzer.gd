7bfb4cc8cb47e27205fbe14ed976e7221fbc5be5
Logic Mindful <master51.main@gmail.com>
2025-08-07 22:28:23 +0200

    progress? (ill model the rest etc)

---
+++ Scripts/Interactable/Buzzer.gd
@@ -0,0 +1,18 @@
+extends Interactable
+
+@onready var animation_player: AnimationPlayer = get_node(^"../../AnimationPlayer")
+
+var is_pressed: bool = false
+
+func get_interaction_text():
+	return "E/LMB : Push"
+
+func primary():
+	if is_pressed:
+		return
+	is_pressed = true
+	animation_player.play(&"press")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	is_pressed = false
