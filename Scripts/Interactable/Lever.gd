be448643e66c125148ba5ff26cfc696c90e3c541
Aarion <aaron.oelschlegel@outlook.com>
2025-08-07 18:37:51 +0200

    adf

--- Scripts/Interactable/Lever.gd
+++ Scripts/Interactable/Lever.gd
@@ -1,6 +1,6 @@
 extends Interactable
 
-@onready var animation_player: AnimationPlayer = get_node(^"../../AnimationPlayer")
+@onready var animation_player: AnimationPlayer = get_node(^"../../../AnimationPlayer")
 
 var is_pressed: bool = false
 var state: bool = false
@@ -14,9 +14,9 @@ func primary():
 	is_pressed = true
 	state = not state
 	if state:
-		animation_player.play(&"lever")
+		animation_player.play(&"switch")
 	else:
-		animation_player.play_backwards(&"lever")
+		animation_player.play_backwards(&"switch")
 
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
