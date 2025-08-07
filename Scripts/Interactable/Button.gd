eea51c704874bfb647f340f3625afab9b2dfa7da
Logic Mindful <master51.main@gmail.com>
2025-08-07 16:22:09 +0200

    idk just some basics i think?

--- Scripts/Interactable/Button.gd
+++ Scripts/Interactable/Button.gd
@@ -2,10 +2,18 @@ extends Interactable
 
 @onready var animation_player: AnimationPlayer = get_node(^"../../AnimationPlayer")
 
+var is_pressed: bool = false
 
 func get_interaction_text():
 	return "interact"
 
 func interact():
+	if is_pressed:
+		return
 	print("interacted with button")
+	is_pressed = true
 	animation_player.play(&"press")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	is_pressed = false
