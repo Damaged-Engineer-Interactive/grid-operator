29be77277d09aedc5b667acf62fff37874e24dd6
Aarion <aaron.oelschlegel@outlook.com>
2025-08-07 17:10:39 +0200

    did some work

---
+++ Scripts/Interactable/Lever.gd
@@ -0,0 +1,24 @@
+extends Interactable
+
+@onready var animation_player: AnimationPlayer = get_node(^"../../AnimationPlayer")
+
+var is_pressed: bool = false
+var state: bool = false
+
+func get_interaction_text():
+	return "switch"
+
+func interact():
+	if is_pressed:
+		return
+	print("interacted with lever")
+	is_pressed = true
+	state = not state
+	if state:
+		animation_player.play(&"lever")
+	else:
+		animation_player.play_backwards(&"lever")
+
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	is_pressed = false
