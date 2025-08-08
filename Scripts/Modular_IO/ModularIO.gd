c5270814a1e6525def97e721ce9238583e541d00
Aarion <aaron.oelschlegel@outlook.com>
2025-08-08 16:01:29 +0200

    Did class things, so im pushing

---
+++ Scripts/Modular_IO/ModularIO.gd
@@ -0,0 +1,17 @@
+class_name ModularIO
+extends Object
+
+func get_functions() -> Array[Callable]:
+	return []
+
+func get_function(name: String) -> Callable:
+	return Callable()
+
+func get_signals() -> Array[Signal]:
+	return []
+	
+func get_signal(name: String) -> Signal:
+	return Signal()
+
+func trigger_signal(name: String) -> void:
+	return
